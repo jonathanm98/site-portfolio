b8bc480ab673cc59df85a8d9cb9adaa236b3e774
jonathanmenelet@hotmail.fr <jmenelet98>
2022-05-12 21:37:33 +0200

    Ajout nav active on scroll

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const allRonds = document.querySelectorAll(".rond");
 const allBoxes = document.querySelectorAll(".box");
 
+const home = document.querySelector("#home");
+const skills = document.querySelector("#skills");
+// const projects = document.querySelector("#skills");
+// const contact = document.querySelector("#skills");
+const allNavs = document.querySelectorAll("header a");
+
 const controller = new ScrollMagic.Controller();
 
 allBoxes.forEach((box) => {
@@ -8,15 +14,35 @@ allBoxes.forEach((box) => {
     if (
       allRonds[i].getAttribute("data-anim") === box.getAttribute("data-anim")
     ) {
-      let tween = gsap.from(box, { y: -50, opacity: 0, duration: 0.5 });
+      let tween = gsap.from(box, {
+        scale: 0.6,
+        y: -50,
+        opacity: 0,
+        duration: 0.5,
+      });
 
       let scene = new ScrollMagic.Scene({
         triggerElement: allRonds[i],
-        reverse: true,
+        reverse: false,
       })
         .setTween(tween)
-        .addIndicators()
         .addTo(controller);
     }
   }
 });
+new ScrollMagic.Scene({
+  triggerElement: home,
+  reverse: true,
+  duration: home.clientHeight,
+})
+  .setClassToggle("#n1", "active")
+  .addIndicators()
+  .addTo(controller);
+new ScrollMagic.Scene({
+  triggerElement: skills,
+  reverse: true,
+  duration: skills.clientHeight,
+})
+  .setClassToggle("#n2", "active")
+  .addIndicators()
+  .addTo(controller);
